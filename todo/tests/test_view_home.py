9ce1558d2db93fa61643810bdbc6fcd7fdb33ca7
Jhonattan <millanjhonattan0@gmail.com>
2020-07-17 19:41:50 -0400

    Add home and sign up tests

---
+++ todo/tests/test_view_home.py
@@ -0,0 +1,81 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from django.urls import reverse, resolve
+from ..forms import TodoItemForm
+from ..models import TodoItem
+from ..views import home
+
+class HomeViewTestCase(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username='user', email='email@email.com', password="password")
+		self.username = 'user'
+		self.password = 'password'
+		self.url = reverse('home')
+
+class HomeViewLoginRequiredTests(HomeViewTestCase):
+	def setUp(self):
+		super().setUp()
+		self.response = self.client.get(self.url)
+
+	def test_redirection(self):
+		login_url = reverse('login')
+		self.assertRedirects(self.response, f'{login_url}?next={self.url}')
+
+class HomeViewTests(HomeViewTestCase):
+	def setUp(self):
+		super().setUp()
+		self.client.login(username=self.username, password=self.password)
+		self.response = self.client.get(self.url)
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_view_function(self):
+		view = resolve('/')
+		self.assertEquals(view.func, home)
+
+	def test_csrf(self):
+		self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+	def test_contains_form(self):
+		form = self.response.context.get('form')
+		self.assertIsInstance(form, TodoItemForm)
+
+	def test_form_inputs(self):
+		self.assertContains(self.response, '<input', 2)
+		self.assertContains(self.response, 'type="text"', 1)
+
+class SuccessfulItemCreationTests(HomeViewTestCase):
+	def setUp(self):
+		super().setUp()
+		self.client.login(username=self.username, password=self.password)
+		data = {
+			'goal': 'Finish my homework'
+		}
+		self.response = self.client.post(self.url, data)
+
+	def test_redirection(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_item_creation(self):
+		self.assertTrue(TodoItem.objects.exists())
+
+	def test_item_display(self):
+		self.assertContains(self.response, f'<li>{TodoItem.objects.first().goal}')
+
+class InvalidItemCreationTests(HomeViewTestCase):
+	def setUp(self):
+		super().setUp()
+		self.client.login(username=self.username, password=self.password)
+		self.response = self.client.post(self.url, {})
+
+	def test_redirection(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_item_creation(self):
+		self.assertFalse(TodoItem.objects.exists())
+
+	def test_form_errors(self):
+		form = self.response.context.get('form')
+		self.assertTrue(form.errors)
---
+++ todo/tests/test_view_signup.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse, resolve
+from ..views import signup
+
+class SignUpViewTests(TestCase):
+	def setUp(self):
+		url = reverse('signup')
+		self.response = self.client.get(url)
+
+	def test_status_code(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_view_function(self):
+		view = resolve('/signup/')
+		self.assertEquals(view.func, signup)
+
+	def test_contains_login_link(self):
+		login_url = reverse('login')
+		self.assertContains(self.response, f'href="{login_url}"')
+
+	def test_csrf(self):
+		self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+	def test_contains_form(self):
+		form = self.response.context.get('form')
+		self.assertIsInstance(form, UserCreationForm)
+
+	def test_form_inputs(self):
+		self.assertContains(self.response, '<input', 5)
+		self.assertContains(self.response, 'type="text"', 1)
+		self.assertContains(self.response, 'type="email"', 1)
+		self.assertContains(self.response, 'type="password"', 2)
+
+class SuccessfulSignUpTests(TestCase):
+	def setUp(self):
+		url = reverse('signup')
+		data = {
+			'username': 'user',
+			'email': 'mail@mail.com',
+			'password1': 'password212121',
+			'password2': 'password212121'
+		}
+		self.response = self.client.post(url, data)
+
+	def test_redirection(self):
+		self.assertRedirects(self.response, reverse('home'))
+
+	def test_user_creation(self):
+		self.assertTrue(User.objects.exists())
+
+	def test_user_authenticaton(self):
+		response = self.client.get(reverse('home'))
+		user = response.context.get('user')
+		self.assertTrue(user.is_authenticated)
+
+class InvalidSignUpTests(TestCase):
+	def setUp(self):
+		url = reverse('signup')
+		self.response = self.client.post(url, {})
+
+	def test_redirection(self):
+		self.assertEquals(self.response.status_code, 200)
+
+	def test_user_creation(self):
+		self.assertFalse(User.objects.exists())
+
+	def test_form_errors(self):
+		form = self.response.context.get('form')
+		self.assertTrue(form.errors)
